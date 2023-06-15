eeb7ed8bcb8d3b533bd5e7d348af3bafd0809a1d
Artur Khachatryan <artur.khachatryan.dev@gmail.com>
2023-06-15 13:24:51 +0400

    connect mongodb to nest, some changes in configurations necessary for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { server, database } from "./config/configuration";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import configuration from "./config/configuration";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [server, database],
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get("DATABASE.MONGODB_URI"),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
